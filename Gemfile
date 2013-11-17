9b9ff5975cbbc23cd399341ed7bef61bba068478
Justin Hill <jhill.d@gmail.com>
2013-11-17 17:55:47 -0500

    - Initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# Webstrap gemfile
+source "https://rubygems.org"
+
+gem 'guard'
+gem 'guard-livereload'
+gem 'heel'
---
+++ bootstrap.sh
@@ -0,0 +1,39 @@
+# Bootstrap a new site.
+
+if [ $1 == "init" ] ; then
+	# create bootstrapping directory
+	mkdir $2
+	cd $2
+
+	# init guard file, hiding unimportant messages. should still
+	# report a bad outcome
+	guard init livereload 2>&1 | grep -v INFO
+
+	# make public dir andcopy other webby bootstrapping things
+	# into this directory
+	mkdir public
+	cp ~/.web-bootstrap/template/* ./public
+
+	# put us back where we were
+	cd ..
+
+	echo "Done bootstrapping!"
+
+elif [ $1 == "serve" ] ; then
+	# start the web server
+	heel -p 8080 -r ./public -d
+
+	# sleep half a second to allow guard to start, then refresh
+	# for livereload
+	(sleep .5 && osascript ~/.web-bootstrap/refresh-safari.scpt)&
+
+	# start guard
+	guard
+
+	# kill the web server
+	heel -k -p 8080
+
+elif [ $1 == "kill" ] ; then
+	heel -k -p 8080
+fi
+
